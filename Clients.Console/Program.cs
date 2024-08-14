c635c02b18b4196e32c2a3f57da757068891e157
Artsem Dziaryd <artemderid@gmail.com>
2024-08-13 20:27:55 -0500

    Init

---
+++ Clients.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+
+var cancellationTokenSource = new CancellationTokenSource();
+
+var path = Path.GetTempFileName();
+var appStatus = string.Empty;
+var sizeInMb = 200;
+
+try
+{
+    FileService fileService = new();
+    await fileService.GenerateTextFile(cancellationTokenSource.Token, path, sizeInMb);
+    appStatus = "Success";
+}
+catch (OperationCanceledException)
+{
+    appStatus = "Canceled";
+}
+finally
+{
+    File.Delete(path);
+    Console.WriteLine(appStatus);
+}
---
+++ Domain/FileService.cs
@@ -0,0 +1,30 @@
+﻿namespace Domain;
+
+public class FileService
+{
+    private const string Alphabet = "abcdefghigklmnopqrstuvwxyz";
+    
+    public async Task GenerateTextFile(CancellationToken token, string path, int lengthMb)
+    {
+        try
+        {
+            await using var writer = new StreamWriter(path);
+
+            var buffer = new char[128];
+            var bufferCount = lengthMb * 1024 * 1024 / buffer.Length;
+
+            for (int i = 0; i < bufferCount; i++)
+            {
+                FillRandomText(buffer);
+                await writer.WriteAsync(buffer, token);
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            File.Delete(path);
+            throw;
+        }
+    }
+
+    private void FillRandomText(Span<char> buffer) => Random.Shared.GetItems(Alphabet.AsSpan(), buffer);
+}
